7f123edb6bff140fc3ab0bfd49ab5876579805f7
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2019-12-13 23:33:13 -0800

    Setting up the Axios connection
    Setting up the connection to the SWAPI API using axios.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component { 
   constructor(props){
@@ -6,14 +7,30 @@ class App extends Component {
 
     this.state = {
       people: [],
-      
+
     }
+
+    this.getPeople = this.getPeople.bind(this);
+  }
+
+  // function to get the information from the API and AXIOS
+  getPeople(){
+    return axios.get("https://swapi.co/api/people")
+    // will return a promise
+    .then((response) => {
+      console.log(response.data.results);
+      this.setState( { people: response.data.results})
+    })
+
+  }
+
+  componentDidMount(){
+    this.getPeople()
   }
 
   render() {
     return(
       <div className='App'>
-        <h1>Star Wars React Project</h1>
       </div>
     );
   }
